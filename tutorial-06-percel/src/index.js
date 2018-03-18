7f7a3d166c7a01d130035e5d433dfff5c334313b
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-03-18 23:18:56 +0900

    added parcel with preact tutorial

---
+++ tutorial-06-percel/src/index.js
@@ -0,0 +1,35 @@
+import { h, render, Component } from 'preact'
+
+class Clock extends Component {
+  constructor () {
+    super()
+    this.state.time = Date.now()
+  }
+
+  componentDidMount () {
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() })
+    }, 1000)
+  }
+
+  componentWillMount () {
+    clearInterval(this.timer)
+  }
+
+  render (props, state) {
+    let time = new Date(state.time).toLocaleString()
+    return (
+      <div>
+        <time>{ time }</time>
+      </div>
+    )
+  }
+}
+
+render((
+  <div class="page">
+    <h1>Hello, preact!</h1>
+    <Clock/>
+    <button onClick={ e => alert('Hi!') }>Click me!</button>
+  </div>
+), document.getElementById('app'))
